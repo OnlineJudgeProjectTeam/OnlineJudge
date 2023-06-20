a3ca08b77d71dbf0cf383d439e6b0a5b7a4e0e60
Hang <1767390619@qq.com>
2023-06-20 17:26:16 +0800

    update UserHolder

--- src/main/java/com/example/onlinejudge/service/impl/ProblemServiceImpl.java
+++ src/main/java/com/example/onlinejudge/service/impl/ProblemServiceImpl.java
@@ -23,11 +23,10 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import javax.management.Query;
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
@@ -65,7 +64,7 @@ public class ProblemServiceImpl extends ServiceImpl<ProblemMapper, Problem> impl
     private long memoryCost;
 
     @Override
-    public RunDto JavaJudge(String code, Integer userId, Integer problemId, Integer number) throws UnsupportedEncodingException {
+    public RunDto JavaJudge(String code, Integer userId, Integer problemId, Integer number) throws IOException {
         User user = userService.QueryById(userId);
         Problem problem = QueryById(problemId);
         String username = user.getUsername();
@@ -76,7 +75,13 @@ public class ProblemServiceImpl extends ServiceImpl<ProblemMapper, Problem> impl
             new File(userPath + "/" + username + "/" + problem.getName() + "/solution").mkdirs();
         }
         String folderName = UUID.randomUUID().toString();
-        new File(userPath + "/" + username + "/" + problem.getName() + "/submission/" + folderName).mkdirs();
+        String codePath = userPath + "/" + username + "/" + problem.getName() + "/submission/" + folderName;
+        new File(codePath).mkdirs();
+        File file1 = new File(codePath + "/" + "code.txt");
+        if (!file1.exists()) {
+            file1.createNewFile();
+            fileService.writeFile(file1.getPath(), code);
+        }
         String userCodePath = userPath + "/" + username + "/" + problem.getName() + "/submission/" + folderName;
         String problemCodePath = problemPath + "/" + problem.getName() + "/java";
         //将测试文件的依赖拷到用户目录下
@@ -97,7 +102,7 @@ public class ProblemServiceImpl extends ServiceImpl<ProblemMapper, Problem> impl
         Integer acNum = user.getAcNum();
         Integer submitNum = user.getSubmitNum();
         user.setSubmitNum(++submitNum);
-        if(!compileResult){
+        if (!compileResult) {
             submission.setPass(Type.notPass);
             submission.setTimeCost(-1L);
             submission.setMemoryCost(-1L);
@@ -105,7 +110,7 @@ public class ProblemServiceImpl extends ServiceImpl<ProblemMapper, Problem> impl
             String message = fileService.readFile(userCodePath + "/stderr.txt");
             user.setAcRate(new BigDecimal(acNum).divide(new BigDecimal(submitNum), 2, BigDecimal.ROUND_HALF_UP));
             userService.update(user);
-            return new RunDto(message, -1L, -1L,new BigDecimal("0"),new BigDecimal("0"));
+            return new RunDto(message, -1L, -1L, new BigDecimal("0"), new BigDecimal("0"));
         }
         //运行
         Boolean runResult = JavaRun(userCodePath, number);
@@ -119,28 +124,28 @@ public class ProblemServiceImpl extends ServiceImpl<ProblemMapper, Problem> impl
             submissionService.save(submission);
             user.setAcRate(new BigDecimal(acNum).divide(new BigDecimal(submitNum), 2, BigDecimal.ROUND_HALF_UP));
             userService.update(user);
-            return new RunDto(message, -1L, -1L,new BigDecimal("0"),new BigDecimal("0"));
+            return new RunDto(message, -1L, -1L, new BigDecimal("0"), new BigDecimal("0"));
         } else {
             submission.setTimeCost(timeCost);
             submission.setMemoryCost(memoryCost);
             submission.setPass(Type.notPass);
-            if(timeCost>problem.getTimeLimit()){
+            if (timeCost > problem.getTimeLimit()) {
                 submissionService.save(submission);
                 user.setAcRate(new BigDecimal(acNum).divide(new BigDecimal(submitNum), 2, BigDecimal.ROUND_HALF_UP));
                 userService.update(user);
-                return new RunDto("Time Limit Exceeded", timeCost, memoryCost,new BigDecimal("0"),new BigDecimal("0"));
+                return new RunDto("Time Limit Exceeded", timeCost, memoryCost, new BigDecimal("0"), new BigDecimal("0"));
             }
             if (memoryCost > problem.getMemoryLimit()) {
                 submissionService.save(submission);
                 user.setAcRate(new BigDecimal(acNum).divide(new BigDecimal(submitNum), 2, BigDecimal.ROUND_HALF_UP));
                 userService.update(user);
-                return new RunDto("Memory Limit Exceeded", timeCost, memoryCost,new BigDecimal("0"),new BigDecimal("0"));
+                return new RunDto("Memory Limit Exceeded", timeCost, memoryCost, new BigDecimal("0"), new BigDecimal("0"));
             }
             String message = fileService.readFile(userCodePath + "/stdout.txt");
-           if( message.length() == RightAnserLenth) {
-               user.setAcNum(++acNum);
-               submission.setPass(Type.pass);
-           }
+            if (message.length() == RightAnserLenth) {
+                user.setAcNum(++acNum);
+                submission.setPass(Type.pass);
+            }
             BigDecimal timeBeat = submissionService.getTimeBeat(problemId, Type.java, timeCost);
             BigDecimal memoryBeat = submissionService.getMemoryBeat(problemId, Type.java, memoryCost);
             submission.setTimeBeat(timeBeat);
@@ -148,7 +153,7 @@ public class ProblemServiceImpl extends ServiceImpl<ProblemMapper, Problem> impl
             submissionService.save(submission);
             user.setAcRate(new BigDecimal(acNum).divide(new BigDecimal(submitNum), 2, BigDecimal.ROUND_HALF_UP));
             userService.update(user);
-            return new RunDto(message, timeCost, memoryCost,timeBeat,memoryBeat);
+            return new RunDto(message, timeCost, memoryCost, timeBeat, memoryBeat);
         }
     }
 
@@ -193,7 +198,7 @@ public class ProblemServiceImpl extends ServiceImpl<ProblemMapper, Problem> impl
     public Boolean JavaCompile(String workingDirectory, Integer number) {
         try {
             // 构建命令
-            ProcessBuilder processBuilder = new ProcessBuilder("javac","-J-Duser.language=en", "-encoding", "utf-8", "./Test" + number + ".java");
+            ProcessBuilder processBuilder = new ProcessBuilder("javac", "-J-Duser.language=en", "-encoding", "utf-8", "./Test" + number + ".java");
             // 设置工作目录
             processBuilder.directory(new File(workingDirectory));
             // 设置输出文件
@@ -251,7 +256,7 @@ public class ProblemServiceImpl extends ServiceImpl<ProblemMapper, Problem> impl
     }
 
     @Override
-    public RunDto CJudge(String code, Integer userId, Integer problemId, Integer number) {
+    public RunDto CJudge(String code, Integer userId, Integer problemId, Integer number) throws IOException {
         User user = userService.QueryById(userId);
         Problem problem = QueryById(problemId);
         String username = user.getUsername();
@@ -261,8 +266,15 @@ public class ProblemServiceImpl extends ServiceImpl<ProblemMapper, Problem> impl
             new File(userPath + "/" + username + "/" + problem.getName() + "/submission").mkdirs();
             new File(userPath + "/" + username + "/" + problem.getName() + "/solution").mkdirs();
         }
-        String folderName = UUID.randomUUID().toString();;
-        new File(userPath + "/" + username + "/" + problem.getName() + "/submission/" + folderName).mkdirs();
+        String folderName = UUID.randomUUID().toString();
+        ;
+        String codePath = userPath + "/" + username + "/" + problem.getName() + "/submission/" + folderName;
+        new File(codePath).mkdirs();
+        File file1 = new File(codePath + "/" + "code.txt");
+        if (!file1.exists()) {
+            file1.createNewFile();
+            fileService.writeFile(file1.getPath(), code);
+        }
         String userCodePath = userPath + "/" + username + "/" + problem.getName() + "/submission/" + folderName;
         String problemCodePath = problemPath + "/" + problem.getName() + "/c";
         //将测试文件的依赖拷到用户目录下
@@ -272,7 +284,7 @@ public class ProblemServiceImpl extends ServiceImpl<ProblemMapper, Problem> impl
         //将测试文件拷到用户目录下
         fileService.fileCopy(problemCodePath + "/Test" + number + ".c", userCodePath + "/Test" + number + ".c", true);
         //编译
-        Boolean compileResult = CCompile(userCodePath,number);
+        Boolean compileResult = CCompile(userCodePath, number);
         Submission submission = new Submission();
         submission.setProblemId(problemId);
         submission.setUserId(userId);
@@ -283,7 +295,7 @@ public class ProblemServiceImpl extends ServiceImpl<ProblemMapper, Problem> impl
         Integer acNum = user.getAcNum();
         Integer submitNum = user.getSubmitNum();
         user.setSubmitNum(++submitNum);
-        if(!compileResult){
+        if (!compileResult) {
             submission.setPass(Type.notPass);
             submission.setTimeCost(-1L);
             submission.setMemoryCost(-1L);
@@ -291,7 +303,7 @@ public class ProblemServiceImpl extends ServiceImpl<ProblemMapper, Problem> impl
             user.setAcRate(new BigDecimal(acNum).divide(new BigDecimal(submitNum), 2, BigDecimal.ROUND_HALF_UP));
             userService.update(user);
             String message = fileService.readFile(userCodePath + "/stderr.txt");
-            return new RunDto(message, -1L, -1L,new BigDecimal("0"),new BigDecimal("0"));
+            return new RunDto(message, -1L, -1L, new BigDecimal("0"), new BigDecimal("0"));
         }
         //运行
         Boolean runResult = CRun(userCodePath, number);
@@ -306,8 +318,8 @@ public class ProblemServiceImpl extends ServiceImpl<ProblemMapper, Problem> impl
             submissionService.save(submission);
             user.setAcRate(new BigDecimal(acNum).divide(new BigDecimal(submitNum), 2, BigDecimal.ROUND_HALF_UP));
             userService.update(user);
-            return new RunDto(message, -1L, -1L,new BigDecimal("0"),new BigDecimal("0"));
-        }else{
+            return new RunDto(message, -1L, -1L, new BigDecimal("0"), new BigDecimal("0"));
+        } else {
             submission.setPass(Type.notPass);
             submission.setTimeCost(timeCost);
             submission.setMemoryCost(memoryCost);
@@ -315,13 +327,13 @@ public class ProblemServiceImpl extends ServiceImpl<ProblemMapper, Problem> impl
                 submissionService.save(submission);
                 user.setAcRate(new BigDecimal(acNum).divide(new BigDecimal(submitNum), 2, BigDecimal.ROUND_HALF_UP));
                 userService.update(user);
-                return new RunDto("Time Limit Exceeded", timeCost, memoryCost,new BigDecimal("0"),new BigDecimal("0"));
+                return new RunDto("Time Limit Exceeded", timeCost, memoryCost, new BigDecimal("0"), new BigDecimal("0"));
             }
             if (memoryCost > problem.getMemoryLimit()) {
                 submissionService.save(submission);
                 user.setAcRate(new BigDecimal(acNum).divide(new BigDecimal(submitNum), 2, BigDecimal.ROUND_HALF_UP));
                 userService.update(user);
-                return new RunDto("Memory Limit Exceeded", timeCost, memoryCost,new BigDecimal("0"),new BigDecimal("0"));
+                return new RunDto("Memory Limit Exceeded", timeCost, memoryCost, new BigDecimal("0"), new BigDecimal("0"));
             }
             String message = fileService.readFile(userCodePath + "/stdout.txt");
             if (message.length() == RightAnserLenth) {
@@ -335,7 +347,7 @@ public class ProblemServiceImpl extends ServiceImpl<ProblemMapper, Problem> impl
             user.setAcRate(new BigDecimal(acNum).divide(new BigDecimal(submitNum), 2, BigDecimal.ROUND_HALF_UP));
             userService.update(user);
             submissionService.save(submission);
-            return new RunDto(message, timeCost, memoryCost,timeBeat,memoryBeat);
+            return new RunDto(message, timeCost, memoryCost, timeBeat, memoryBeat);
         }
     }
 
@@ -373,7 +385,7 @@ public class ProblemServiceImpl extends ServiceImpl<ProblemMapper, Problem> impl
             //Windows操作系统
             if (os != null && os.toLowerCase().startsWith("windows")) {
                 processBuilder = new ProcessBuilder(workingDirectory + "/" + "Test" + number);
-            } else{//Linux操作系统
+            } else {//Linux操作系统
                 processBuilder = new ProcessBuilder("./" + "Test" + number);
             }
             // 构建命令
@@ -431,13 +443,13 @@ public class ProblemServiceImpl extends ServiceImpl<ProblemMapper, Problem> impl
     public PageInfo<Problem> getProblemList(Integer pageNum, Integer pageSize, Integer navSize, String name, String tags, String difficulty) {
         PageHelper.startPage(pageNum, pageSize);
         LambdaQueryWrapper<Problem> problemLambdaQueryWrapper = new LambdaQueryWrapper<>();
-        if (name != null &&!name.equals("")&& !name.equals("undefined")) {
+        if (name != null && !name.equals("") && !name.equals("undefined")) {
             problemLambdaQueryWrapper.like(Problem::getName, name);
         }
-        if (tags != null && !tags.equals("")&& !tags.equals("undefined")) {
+        if (tags != null && !tags.equals("") && !tags.equals("undefined")) {
             problemLambdaQueryWrapper.like(Problem::getTags, tags);
         }
-        if (difficulty != null && !difficulty.equals("")&& !difficulty.equals("undefined")) {
+        if (difficulty != null && !difficulty.equals("") && !difficulty.equals("undefined")) {
             problemLambdaQueryWrapper.eq(Problem::getDifficulty, difficulty);
         }
         List<Problem> list = list(problemLambdaQueryWrapper);
@@ -447,9 +459,9 @@ public class ProblemServiceImpl extends ServiceImpl<ProblemMapper, Problem> impl
             favoriteLambdaQueryWrapper.eq(Favorite::getProblemId, problem.getId());
             favoriteLambdaQueryWrapper.eq(Favorite::getUserId, user.getId());
             Favorite one = favoriteService.getOne(favoriteLambdaQueryWrapper);
-            if(one!=null){
+            if (one != null) {
                 problem.setIsFavorite(Type.favorite);
-            }else{
+            } else {
                 problem.setIsFavorite(Type.notFavorite);
             }
             LambdaQueryWrapper<Submission> submissionLambdaQueryWrapper = new LambdaQueryWrapper<>();
@@ -461,13 +473,12 @@ public class ProblemServiceImpl extends ServiceImpl<ProblemMapper, Problem> impl
             } else {
                 Integer isPass = 0;
                 for (Submission submission : submissionList) {
-                    if(submission.getPass()== 1)
-                    {
+                    if (submission.getPass() == 1) {
                         isPass = 1;
                         break;
                     }
                 }
-                problem.setStatus(isPass +1);
+                problem.setStatus(isPass + 1);
             }
         }
         return new PageInfo<>(list, navSize);
@@ -514,14 +525,14 @@ public class ProblemServiceImpl extends ServiceImpl<ProblemMapper, Problem> impl
         return fileService.readFile(templatePath);
     }
 
-    public void isProblemFavorite(Problem problem){
+    public void isProblemFavorite(Problem problem) {
         LambdaQueryWrapper<Favorite> favoriteLambdaQueryWrapper = new LambdaQueryWrapper<>();
         favoriteLambdaQueryWrapper.eq(Favorite::getProblemId, problem.getId());
         favoriteLambdaQueryWrapper.eq(Favorite::getUserId, UserHolder.getUser().getId());
         Favorite one = favoriteService.getOne(favoriteLambdaQueryWrapper);
-        if(one!=null){
+        if (one != null) {
             problem.setIsFavorite(Type.favorite);
-        }else{
+        } else {
             problem.setIsFavorite(Type.notFavorite);
         }
     }
--- src/main/java/com/example/onlinejudge/service/impl/SubmissionServiceImpl.java
+++ src/main/java/com/example/onlinejudge/service/impl/SubmissionServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.onlinejudge.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.onlinejudge.dto.ACData;
 import com.example.onlinejudge.dto.SubmissionDto;
 import com.example.onlinejudge.entity.Problem;
@@ -9,9 +10,7 @@ import com.example.onlinejudge.entity.User;
 import com.example.onlinejudge.mapper.ProblemMapper;
 import com.example.onlinejudge.mapper.SubmissionMapper;
 import com.example.onlinejudge.service.FileService;
-import com.example.onlinejudge.service.IProblemService;
 import com.example.onlinejudge.service.ISubmissionService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.onlinejudge.service.IUserService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -27,7 +26,7 @@ import java.util.List;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author diandianjun
@@ -52,8 +51,8 @@ public class SubmissionServiceImpl extends ServiceImpl<SubmissionMapper, Submiss
             LambdaQueryWrapper<Submission> submissionLambdaQueryWrapper = new LambdaQueryWrapper<>();
             submissionLambdaQueryWrapper.eq(Submission::getUserId, userId);
             submissionLambdaQueryWrapper.eq(Submission::getPass, 1);
-            if(!difficulty.equals("")){
-                submissionLambdaQueryWrapper.eq(Submission::getDifficulty,difficulty);
+            if (!difficulty.equals("")) {
+                submissionLambdaQueryWrapper.eq(Submission::getDifficulty, difficulty);
             }
             List<Submission> list = this.list(submissionLambdaQueryWrapper);
             ACData.setAcNum(list.size());
@@ -61,13 +60,13 @@ public class SubmissionServiceImpl extends ServiceImpl<SubmissionMapper, Submiss
         {
             LambdaQueryWrapper<Submission> submissionLambdaQueryWrapper = new LambdaQueryWrapper<>();
             submissionLambdaQueryWrapper.eq(Submission::getUserId, userId);
-            if(!difficulty.equals("")){
-                submissionLambdaQueryWrapper.eq(Submission::getDifficulty,difficulty);
+            if (!difficulty.equals("")) {
+                submissionLambdaQueryWrapper.eq(Submission::getDifficulty, difficulty);
             }
             List<Submission> list = this.list(submissionLambdaQueryWrapper);
             ACData.setSubmitNum(list.size());
         }
-        if(ACData.getSubmitNum() == 0){
+        if (ACData.getSubmitNum() == 0) {
             ACData.setAcRate(new BigDecimal(0));
             return ACData;
         }
@@ -78,76 +77,75 @@ public class SubmissionServiceImpl extends ServiceImpl<SubmissionMapper, Submiss
     }
 
     @Override
-    public PageInfo<SubmissionDto> getSubmissionList(Integer userId, Integer pageNum, Integer pageSize, Integer navSize, Integer language, String difficulty, Integer pass, LocalDateTime startTime, LocalDateTime endTime,Integer problemId) {
+    public PageInfo<SubmissionDto> getSubmissionList(Integer userId, Integer pageNum, Integer pageSize, Integer navSize, Integer language, String difficulty, Integer pass, LocalDateTime startTime, LocalDateTime endTime, Integer problemId) {
         LambdaQueryWrapper<Submission> submissionLambdaQueryWrapper = new LambdaQueryWrapper<>();
-        submissionLambdaQueryWrapper.eq(Submission::getUserId,userId);
+        submissionLambdaQueryWrapper.eq(Submission::getUserId, userId);
         User user = userService.getById(userId);
-        if(language != null){
-            submissionLambdaQueryWrapper.eq(Submission::getLanguage,language);
+        if (language != null) {
+            submissionLambdaQueryWrapper.eq(Submission::getLanguage, language);
         }
-        if(difficulty != null && !difficulty.equals("")){
-            submissionLambdaQueryWrapper.eq(Submission::getDifficulty,difficulty);
+        if (difficulty != null && !difficulty.equals("")) {
+            submissionLambdaQueryWrapper.eq(Submission::getDifficulty, difficulty);
         }
-        if(pass != null){
-            submissionLambdaQueryWrapper.eq(Submission::getPass,pass);
+        if (pass != null) {
+            submissionLambdaQueryWrapper.eq(Submission::getPass, pass);
         }
-        if(startTime != null){
-            submissionLambdaQueryWrapper.ge(Submission::getExecutionTime,startTime);
+        if (startTime != null) {
+            submissionLambdaQueryWrapper.ge(Submission::getExecutionTime, startTime);
         }
-        if(endTime != null){
-            submissionLambdaQueryWrapper.le(Submission::getExecutionTime,endTime);
+        if (endTime != null) {
+            submissionLambdaQueryWrapper.le(Submission::getExecutionTime, endTime);
         }
-        if(problemId != null){
-            submissionLambdaQueryWrapper.eq(Submission::getProblemId,problemId);
+        if (problemId != null) {
+            submissionLambdaQueryWrapper.eq(Submission::getProblemId, problemId);
         }
         ArrayList<SubmissionDto> submissionDtos = new ArrayList<>();
         HashMap<Integer, Problem> problemTitleMap = problemMapper.getProblemMap();
-        PageHelper.startPage(pageNum,pageSize);
+        PageHelper.startPage(pageNum, pageSize);
         for (Submission submission : this.list(submissionLambdaQueryWrapper)) {
             SubmissionDto submissionDto = new SubmissionDto(submission);
             Problem problem = problemTitleMap.get(submission.getProblemId());
             submissionDto.setProblemName(problem.getName());
-            String userCodePath = userPath + "/" + user.getUsername() + "/" + problem.getName() + "/submission/"+submission.getFolderName();
-            String code;
-            if(submission.getLanguage()==0){
-                code = fileService.readFile(userCodePath + "/Test1.java");
-            }else{
-                code = fileService.readFile(userCodePath + "/Test1.c");
-            }
+            String userCodePath = userPath + "/" + user.getUsername() + "/" + problem.getName() + "/submission/" + submission.getFolderName();
+            String code = fileService.readFile(userCodePath + "/code.txt");
             submissionDto.setCode(code);
             String output;
-            if(submission.getPass() == 1){
+            if (submission.getPass() == 1) {
                 output = fileService.readFile(userCodePath + "/stdout.txt");
-            }else{
+            } else {
                 output = fileService.readFile(userCodePath + "/stderr.txt");
             }
             submissionDto.setOutput(output);
             submissionDtos.add(submissionDto);
         }
-        return new PageInfo<>(submissionDtos,navSize);
+        return new PageInfo<>(submissionDtos, navSize);
     }
 
     @Override
     public BigDecimal getTimeBeat(Integer problemId, Integer language, Long timeCost) {
         LambdaQueryWrapper<Submission> submissionLambdaQueryWrapper = new LambdaQueryWrapper<>();
-        submissionLambdaQueryWrapper.eq(Submission::getProblemId,problemId);
-        submissionLambdaQueryWrapper.eq(Submission::getLanguage,language);
-        submissionLambdaQueryWrapper.eq(Submission::getPass,1);
+        submissionLambdaQueryWrapper.eq(Submission::getProblemId, problemId);
+        submissionLambdaQueryWrapper.eq(Submission::getLanguage, language);
+        submissionLambdaQueryWrapper.eq(Submission::getPass, 1);
         long total = count(submissionLambdaQueryWrapper);
-        submissionLambdaQueryWrapper.ge(Submission::getExecutionTime,timeCost);
+        if (total == 0)
+            return new BigDecimal(0);
+        submissionLambdaQueryWrapper.ge(Submission::getExecutionTime, timeCost);
         long better = count(submissionLambdaQueryWrapper);
-        return new BigDecimal(better).divide(new BigDecimal(total),2,BigDecimal.ROUND_HALF_UP);
+        return new BigDecimal(better).divide(new BigDecimal(total), 2, BigDecimal.ROUND_HALF_UP);
     }
 
     @Override
     public BigDecimal getMemoryBeat(Integer problemId, Integer language, Long memoryCost) {
         LambdaQueryWrapper<Submission> submissionLambdaQueryWrapper = new LambdaQueryWrapper<>();
-        submissionLambdaQueryWrapper.eq(Submission::getProblemId,problemId);
-        submissionLambdaQueryWrapper.eq(Submission::getLanguage,language);
-        submissionLambdaQueryWrapper.eq(Submission::getPass,1);
+        submissionLambdaQueryWrapper.eq(Submission::getProblemId, problemId);
+        submissionLambdaQueryWrapper.eq(Submission::getLanguage, language);
+        submissionLambdaQueryWrapper.eq(Submission::getPass, 1);
         long total = count(submissionLambdaQueryWrapper);
-        submissionLambdaQueryWrapper.ge(Submission::getMemoryCost,memoryCost);
+        if (total == 0)
+            return new BigDecimal(0);
+        submissionLambdaQueryWrapper.ge(Submission::getMemoryCost, memoryCost);
         long better = count(submissionLambdaQueryWrapper);
-        return new BigDecimal(better).divide(new BigDecimal(total),2,BigDecimal.ROUND_HALF_UP);
+        return new BigDecimal(better).divide(new BigDecimal(total), 2, BigDecimal.ROUND_HALF_UP);
     }
 }
